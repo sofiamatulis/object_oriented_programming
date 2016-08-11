492067b5b20610c4134e9edbfe6fb9787eee5928
sofiamatulis <sosomatulis@hotmail.com>
2016-08-11 14:47:13 -0400

    added the delivery part of the exercise and the report

--- paperboy/paperboy.rb
+++ paperboy/paperboy.rb
@@ -1,7 +1,7 @@
 class Paperboy
 
 
-    attr_reader :name, :experience, :side, :earnings
+    attr_reader :name, :experience, :earnings
 
     def initialize (name,experience,side,earnings)
       @name= name
@@ -13,9 +13,35 @@ class Paperboy
     end
 
 
+    def side
+      if @side % 2 == 0
+        puts "even"
+      else
+        puts "odd"
+    end
+  end
+
     def quota
       50 + @experience/2
     end
+
+    def deliver (start_address, end_address)
+     if quota = 50
+       then (@end_address - @start_address) * 0.25
+     elsif quota > 50
+       then (50*0.25 + (quota - 50)*0.5)
+     elsif quota < 50
+       then (50*0.25)-2
+     end
+   end
+
+
+def report
+
+  puts "I'm #{name} ,I've delivered  #{quota} papers and I've earned #{deliver} so far"
+end
+
+
 
 
 
