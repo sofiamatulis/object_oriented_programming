89aa3c97a4ee0cc72725e22998907bf1f10bd236
sofiamatulis <sosomatulis@hotmail.com>
2016-08-11 14:08:53 -0400

    added the meal between 0 and 23 and the isntances correctly

--- cat/cat.rb
+++ cat/cat.rb
@@ -1,18 +1,35 @@
 class Cat
   attr_reader :name, :preferred_food, :meal_time, :eats_at
 
-  def initialize (name,preferred_food,meal_time, eats_at)
+  def initialize (name,preferred_food,meal_time)
     @name= name
     @preferred_food = preferred_food
-    @meal_time = meal time
-    @eats_at= eats_at
+    @meal_time = meal_time
+
+
 
   end
 
+def eats_at
+  if  meal_time <= 23 && meal_time >= 0
+    return @meal_time
+  end
+end
 
 
-  b = Cat.new ("Bob","fish",5,"11AM")
-  m = Cat.new ("Mary","rats",7,"3PM")
+
+
+def meow
+  puts "My name is #{name} and I eat #{preferred_food} at #{eats_at}"
+end
+
+
 
 
 end
+
+  b = Cat.new('Bob','fish',20)
+  m = Cat.new('Mary','rats',7)
+
+  b.meow
+  m.meow
