48825f80e16ef6b3c5cb6d3a5eb6f3dca59fd2c8
sofiamatulis <sosomatulis@hotmail.com>
2016-08-11 14:19:57 -0400

    added the attributes of paperboy

---
+++ paperboy/paperboy.rb
@@ -0,0 +1,13 @@
+class Paperboy
+
+
+    attr_reader :name, :experience, :side, :earnings
+
+    def initialize (name,experience,side,earnings)
+      @name= name
+      @experience=experience
+      @side=side
+      @earnings=earnings
+    
+
+    end
