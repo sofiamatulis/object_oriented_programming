3d5d575bfefda684556a44bbe9865c57e70cbb03
sofiamatulis <sosomatulis@hotmail.com>
2016-08-11 13:33:24 -0400

    added what time the cat eats at

--- cat/cat.rb
+++ cat/cat.rb
@@ -1,15 +1,18 @@
 class Cat
-  def initialize (name,preferred_food,meal_time)
+  attr_reader :name, :preferred_food, :meal_time, :eats_at
+
+  def initialize (name,preferred_food,meal_time, eats_at)
     @name= name
     @preferred_food = preferred_food
     @meal_time = meal time
+    @eats_at= eats_at
 
   end
 
 
 
-  b = Cat.new ("Bob","fish",5)
-  m = Cat.new ("Mary","rats",20)
+  b = Cat.new ("Bob","fish",5,"11AM")
+  m = Cat.new ("Mary","rats",7,"3PM")
 
 
 end
