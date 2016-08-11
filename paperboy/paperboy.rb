540e2d2ad96c355394c50d4a2c5fe45ea74341ef
sofiamatulis <sosomatulis@hotmail.com>
2016-08-11 17:14:03 -0400

    paperboy works and has been updated with the report and extra ends! changed paper delivered to experience

--- paperboy/paperboy.rb
+++ paperboy/paperboy.rb
@@ -14,49 +14,34 @@ class Paperboy
 
 
 
+
     def deliver(start_address, end_address)
-      paper_delivered = 0
-      if side == "odd"
-        start_address..end_address.each do |address|
+      if @side == "odd"
+        (start_address..end_address).each do |address|
           if address % 2 != 0
-             paper_delivered += 1
+             @experience += 1
            end
          end
        end
-       if side == "even"
-         start_address..end_address.each do |address|
+
+       if @side == "even"
+         (start_address..end_address).each do |address|
            if address % 2 == 0
-             paper_delivered += 1
+             @experience += 1
            end
          end
         end
 
-      if (papers_delivered- @quota )== 50
-        then
-        @earnings = papers_delivered * 0.25
-      elsif papers_delivered > 50
-        then
-        @earnings = papers_delivered
-
+      expected_quota = quota
+      if (@experience - @experience) == 0
+        @earnings =  @experience * 0.25
+      elsif (@experience- expected_quota) > 0
+        @earnings = (@experience- expected_quota) * 0.5 + (expected_quota * 0.25 )
+      else
+        @earnings = (@experience * 0.25) -2
+      end
+      return @earnings
     end
-
-
-  def earnings
-
-    return @earnings
-  end
-
-
-  def update_earnings
-
-    total = @houses_delivered - quota
-
-
-    if total < 0
-      then updates_earnings - 2
-    elsif  total > 0
-      then updated_earnings + 0.5 * (@houses_delivered - quota)
-
 
 
 
@@ -66,21 +51,14 @@ class Paperboy
     50 + (@experience/2)
   end
 
+  def report
 
-
-
-
-
-
-
-def report
-
-  puts "I'm #{name} ,I've delivered  #{quota} papers and I've earned #{deliver} so far"
-end
-
-
+    puts "I'm #{@name} ,I've delivered  #{@experience} papers and I've earned $ #{@earnings} so far"
+  end
 
 end
 
 tommy = Paperboy.new("Tommy", "even")
-puts tommy.experience
+puts tommy.deliver(100,200)
+
+puts tommy.report
