3937ecef96687a5eacc8b8f3455f37e006110f82
sofiamatulis <sosomatulis@hotmail.com>
2016-08-12 12:30:54 -0400

    created class person

--- people.rb
+++ people.rb
@@ -1,11 +1,18 @@
-class Student
+
+class Person
+  def initialize (name)
+    @name=name
+
+class Student < Person
   def learn
     "I get it"
   end
 end
 
-class Instructor
+class Instructor < Person
   def teach
     "Everything in Ruby is an Object"
   end
+end
+
 end
