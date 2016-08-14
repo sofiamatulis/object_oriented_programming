c2d0ce9f1ae134253d830f10f215e3e1f9837505
sofiamatulis <sosomatulis@hotmail.com>
2016-08-14 18:06:46 -0400

    added the variable quantity

--- item1.rb
+++ item1.rb
@@ -4,6 +4,7 @@ class Item
 
   attr_reader :name
   attr_reader :price
+  attr_reader :quantity
 
 
 
@@ -20,8 +21,8 @@ class Item
 
 #quantity
 
-  def initialize (name,price,category,imported)
-  # @quantity =+ 1
+  def initialize (quantity,name,price,category,imported)
+  @quantity = quantity
   @name = name
   @price = price
   @category = category
@@ -42,21 +43,25 @@ class Item
 
   if (@imported == false) && (@category == "food" || @category == "medical" || @category == "book")
     #  @@sales_tax_value = 0
-    @@total_value += @price
+    @@total_value += (@price * @quantity)
+    @price * @quantity
 
   elsif (@imported == false) && (@category == "other")
-    @@sales_tax_value = @price * 0.10
-    @@total_value += @price * 1.10
+    @@sales_tax_value += (@price * 0.10 * @quantity)
+    @@total_value += (@price * 1.10 * @quantity)
+    @price * 1.10 * @quantity
 
 
   elsif (@imported == true) && (@category == "food" || @category == "medical" || @category == "book")
-       @@sales_tax_value = @price * 0.05
-      @@total_value += @price * 1.05
+       @@sales_tax_value += (@price * 0.05 * @quantity)
+      @@total_value += (@price * 1.05 * @quantity)
+      @price * 1.05 * @quantity
 
 
   elsif (@imported == true) && (@category == "other")
-        @@sales_tax_value = @price * 0.10 * 0.05
-        @@total_value += @price * 1.10 * 1.05
+        @@sales_tax_value += (@price * 0.10 * 0.05 * @quantity)
+        @@total_value += (@price * 1.10 * 1.05 * @quantity)
+        @price * 1.10 * 1.05 * @quantity
   end
 
  end
@@ -67,20 +72,23 @@ end
 
 
 
-item1= Item.new("Harry Potter", 10, "book",false)
-item2 = Item.new("Banana", 3, "food",false)
-item3 = Item.new("imported table", 11,"other",true)
+item1= Item.new(1,"Harry Potter", 10.00 , "book",false)
+item2 = Item.new(2,"Banana", 3.45, "food",false)
+item3 = Item.new(3,"imported table", 11.11,"other",true)
 
-list_of_total_items = {
-  item1.name => item1.tax,
-  item2.name => item2.tax,
-  item3.name => item3.tax
+list_items = {
+  item1.quantity => [item1.name, item1.tax],
+  item2.quantity => [item2.name,item2.tax],
+  item3.quantity => [item3.name, item3.tax]
+
 }
 
 
-receipt1 = Receipt.new(list_of_total_items)
+
+
+receipt1 = Receipt.new(list_items)
 
 receipt1.print_receipt
 
-puts "Sales Tax: $#{ Item.sales_tax_value}"
-puts "Total Bill: $#{ Item.total_value}"
+puts "Taxes: $#{Item.sales_tax_value}"
+puts "Total: $#{Item.total_value}"
